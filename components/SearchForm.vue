5e5be63d6848765ce2a2966fef5af1b189be8d61
gafilipoie <marius.afilipoie@gmail.com>
2024-01-24 19:08:05 +0200

    moved local styles to global styles

--- components/SearchForm.vue
+++ components/SearchForm.vue
@@ -1,31 +1,25 @@
 <template>
   <div class="search">
-    <label class="search__label" for="doggie-search">Token ID</label>
+    <label for="doggie-search">Token ID</label>
+
     <input
       v-model="search"
-      class="search__input"
-      :class="{ 'search__input--disabled': isLoading }"
+      :class="{ '--disabled': isLoading }"
       type="text"
       name="search"
       autocomplete="false"
       :disabled="isLoading"
       required
     />
+
+    <span v-if="error" class="search__error">{{ error }}</span>
+
     <div class="search__cta">
-      <button
-        class="search__cta__button search__cta__button--outlined"
-        :class="{ 'search__cta__button--disabled': isLoading }"
-        @click="handleOnRandom()"
-      >
+      <button class="--outlined" :class="{ '--disabled': isLoading }" :disabled="isLoading" @click="handleOnRandom()">
         Random Search
       </button>
-      <button
-        class="search__cta__button"
-        :class="{ 'search__cta__button--disabled': isLoading }"
-        @click="handleOnSearch(search)"
-      >
-        Search
-      </button>
+
+      <button :class="{ '--disabled': isLoading }" :disabled="isLoading" @click="handleOnSearch(search)">Search</button>
     </div>
   </div>
 </template>
@@ -47,9 +41,14 @@ defineProps({
     type: Function,
     required: true,
   },
+  error: {
+    type: String,
+    required: false,
+    default: '',
+  },
 });
 
-const search = ref('');
+const search = ref(6314n);
 </script>
 
 <style lang="scss" scoped>
@@ -59,76 +58,12 @@ const search = ref('');
   gap: 12px;
   width: 300px;
   margin: 24px 0;
-  &__label {
-    color: #3a3b3f;
-    font-size: 16px;
-    line-height: 22px;
-  }
-  &__input {
-    width: 100%;
-    height: 36px;
-    padding: 0 22px 0 12px;
-    outline: none;
-    color: #8f8f8f;
-    font-size: 12px;
-    border: 1px solid #cccccc;
-    border-radius: $border-radius;
-    background-color: $basic-white;
-    &:hover {
-      box-shadow: $box-shadow;
-    }
-    &:focus {
-      border: 1px solid #27aae2;
-    }
-    &--disabled {
-      color: #c4cace;
-      cursor: not-allowed;
-      &:hover {
-        box-shadow: none;
-      }
-    }
+  &__error {
+    color: $error;
   }
   &__cta {
     display: flex;
     gap: 16px;
-    &__button {
-      width: 100%;
-      padding: 10px;
-      cursor: pointer;
-      text-align: center;
-      color: $basic-white;
-      font-size: 12px;
-      line-height: 12px;
-      border: 1px solid transparent;
-      border-radius: $border-radius;
-      background-color: $button-color;
-      transition: 0.1s linear background-color;
-      &:hover {
-        background-color: $button-color-hover;
-      }
-      &:active {
-        background-color: $button-color-active;
-      }
-      &--disabled {
-        color: $basic-white;
-        background-color: #9fd8f1;
-        pointer-events: none;
-        cursor: not-allowed;
-      }
-      &--outlined {
-        color: #006aac;
-        border-color: #006aac;
-        background-color: transparent;
-        &:hover {
-          color: $basic-white;
-          background-color: #006aac;
-        }
-        &--disabled {
-          color: #9fd8f1;
-          border: 1px solid #9fd8f1;
-        }
-      }
-    }
   }
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <ul>
-          <li><NuxtLink to="/">Home</NuxtLink></li>
-          <li><NuxtLink to="/doggies">Doggies</NuxtLink></li>
+    <header class="header">
+      <nav class="header__nav">
+        <ul class="header__nav__list">
+          <li class="header__nav__list__item"><NuxtLink to="/">Home</NuxtLink></li>
+          <li class="header__nav__list__item"><NuxtLink to="/doggies">Doggies</NuxtLink></li>
         </ul>
       </nav>
+
+      <div class="connection-status-wrapper">
+        <ConnectionStatus />
+      </div>
     </header>
 
     <div>
@@ -16,22 +20,31 @@
 </template>
 
 <style lang="scss" scoped>
-nav {
-  padding: 0 1rem;
-  ul {
-    list-style-type: none;
-    padding: 0;
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    li {
-      margin: 0 0.5rem;
-      padding: 0.25rem;
-      font-size: 1.2rem;
-      .router-link-exact-active {
-        color: $link-active;
+.header {
+  position: relative;
+  .header__nav {
+    padding: 0 1rem;
+    .header__nav__list {
+      list-style-type: none;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
+      .header__nav__list__item {
+        margin: 0 0.5rem;
+        padding: 0.25rem;
+        font-size: 1.2rem;
+        .router-link-exact-active {
+          color: $link-active;
+        }
       }
     }
+  }
+
+  .connection-status-wrapper {
+    position: absolute;
+    top: 2px;
+    right: 16px;
   }
 }
 </style>
